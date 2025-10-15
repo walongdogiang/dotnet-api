bc3617294e3453597065b530dbc7d5b0f295ac54
walongdogiang <hieuvule1412@gmail.com>
2025-10-15 16:34:11 +0700

    db-docker

--- Auth/Auth.Api/obj/Debug/net9.0/Auth.Api.AssemblyInfo.cs
+++ Auth/Auth.Api/obj/Debug/net9.0/Auth.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Auth.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5538481f0eac9420e9cfd4911dccc6a20a98537c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e6aa255d02aa2c5409bf9c298791843d9cc0c4d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Auth.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Auth.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
