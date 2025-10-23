368a407e023815612ca802c2198ca0a4dc220b23
walongdogiang <hieuvule1412@gmail.com>
2025-10-23 18:26:18 +0700

    create-redis

--- Demo/Demo.Svc/obj/Debug/net9.0/Demo.Svc.AssemblyInfo.cs
+++ Demo/Demo.Svc/obj/Debug/net9.0/Demo.Svc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Demo.Svc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc3617294e3453597065b530dbc7d5b0f295ac54")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31ecafd1f62bda3bf142077afa093f3f71947f46")]
 [assembly: System.Reflection.AssemblyProductAttribute("Demo.Svc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Demo.Svc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
